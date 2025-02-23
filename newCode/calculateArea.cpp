f6ced57c7904762997353b2b70e3d2de5a0750db
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-02-24 01:48:25 +0600

    first commit

---
+++ newCode/calculateArea.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int length = 5, width = 7, area;
+    area = length * width;
+
+    cout<<"Length is: "<<length<<endl;
+    cout<<"Width is: "<<width<<endl;
+    cout<<"Area is: "<<area<<"\n";
+}
---
+++ newCode/second.cpp
@@ -0,0 +1,14 @@
+/**
+ * we will do some
+ * magic using
+ * C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y, z;
+    x = y = z = 10;
+    cout<<"Total sum is: "<<z;
+}
